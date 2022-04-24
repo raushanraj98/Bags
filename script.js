11d9d32173975f0a69e709ba529b3faf49fe2e73
Raushan Raj <roushancri@gmail.com>
2022-04-24 13:32:04 +0530

    search functionality improved

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const search = document.querySelector('.header__search');
 const btn = document.querySelector('main .btn.dropdown-toggle');
 const searchTwo = document.querySelector('.main__search--two');
 search.onclick = function() {
-    btn.style.display = 'none';
-    searchTwo.style.display = 'block';
+    btn.classList.toggle('btn');
+    btn.classList.toggle('not-visible');
+    btn.classList.toggle('dropdown-toggle');
+    searchTwo.classList.toggle("visible"); 
 };
